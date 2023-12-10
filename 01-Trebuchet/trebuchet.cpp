38ccf43cb1896093adb548dd5fcbb14cd6ed8cc8
Justin Danielson <justin@jdanielson.us>
2023-12-10 16:10:38 -0600

    Day one Part two completed

--- 01-Trebuchet/trebuchet.cpp
+++ 01-Trebuchet/trebuchet.cpp
@@ -5,7 +5,22 @@ using namespace std;
 
 int main (){
     int calibrationNumber = 0;
+    string spelledNumbers[10] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
     for (string line; getline(cin, line);) {
+        //Replace spelled out numbers with digits
+        string originalLine = line;
+        for (size_t i = 0; i < 10; i++)
+        {
+            size_t pos = 0;
+            pos = line.find(spelledNumbers[i],pos);
+            while(pos!=string::npos){
+                //line.replace(line.find(spelledNumbers[i]),spelledNumbers[i].length(),to_string(i));
+                line.insert(pos+2,to_string(i));
+                pos = line.find(spelledNumbers[i]);
+            }
+        }
+        cout << "Original Line was: " << originalLine << " Replaced line is: " << line << " --- ";
+
         size_t firstPos = line.find_first_of("0123456789");
         size_t lastPos = line.find_last_of("0123456789");
         //int lineValue = 10 * line[firstPos] + 1 * line[lastPos];
@@ -14,6 +29,9 @@ int main (){
         int  firstDigit = firstChar - '0';
         int lastDigit = lastChar - '0';
         int lineValue = 10 * firstDigit + lastDigit;
+
+        cout << "First digit is: " << firstDigit << " Last digit is: " << lastDigit << " Line Value is: " << lineValue << endl;
+
         calibrationNumber += lineValue;
     }
     cout << "Sum of Calibration Values is: " << calibrationNumber << endl;
